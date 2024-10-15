39133a0f14a19b420ba490f9b6b1068be944cc31
Your Name <jasonbataillon@gmail.com>
2024-10-15 12:24:47 -0400

    Finished seeding the database.

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,8 +1,30 @@
-const prisma = require("../prisma");
+const prisma = require('../prisma');
 
-const seed = async () => {
-  // TODO: this function
+const seed = async (numRestaurants = 3, numCustomers = 5) => {
+  const restaurants = Array.from({ length: numRestaurants }, (_, i) => ({
+    name: `Restaurant ${i + 1}`,
+  }));
+  await prisma.restaurant.createMany({ data: restaurants });
+  const customers = Array.from({ length: numCustomers }, (_, i) => ({
+    name: `Customer ${i + 1}`,
+    email: `customer${i + 1}@anything.com`,
+  }));
+  await prisma.customer.createMany({ data: customers });
 };
+
+for (let i = 0; i < numReservations; i++) {
+  const partySize = 1 + Math.floor(Math.random() * 3);
+  const party = Array.from({ length: partySize }, () => ({
+    id: 1 + Math.floor(Math.random() * numCustomers),
+  }));
+  await prisma.reservation.create({
+    data: {
+      date: new Date().now().toDateString(),
+      restaurantId: 1 + Math.floor(Math.random() * numRestaurants),
+      party: { connect: party },
+    },
+  });
+}
 
 seed()
   .then(async () => await prisma.$disconnect())
