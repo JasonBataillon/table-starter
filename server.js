698d632613c8cecf8e638cdb698d32d4465e23c1
Your Name <jasonbataillon@gmail.com>
2024-10-15 12:36:23 -0400

    Finishes server.js file.

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-const express = require("express");
+const express = require('express');
+const prisma = require('./prisma');
 const app = express();
 const PORT = 3000;
 
-app.use(require("morgan")("dev"));
+app.use(require('morgan')('dev'));
 app.use(express.json());
 
-// TODO: POST /reservations
+app.post('/reservations', async (req, res, next) => {
+  try {
+    const { date, restaurantId, customerIds } = req.body;
+
+    // This convert an array of ids into a shape needed for `connect`
+    const party = customerIds.map((id) => ({ id: +id }));
+    const reservation = await prisma.reservation.create({
+      data: {
+        date,
+        restaurantId: +restaurantId,
+        party: { connect: party },
+      },
+      include: {
+        restaurant: true,
+        party: true,
+      },
+    });
+    res.status(201).json(reservation);
+  } catch (e) {
+    next(e);
+  }
+});
 
 app.use((req, res, next) => {
-  next({ status: 404, message: "Endpoint not found." });
+  next({ status: 404, message: 'Endpoint not found.' });
 });
 
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(err.status ?? 500);
-  res.json(err.message ?? "Sorry, something broke :(");
+  res.json(err.message ?? 'Sorry, something broke :(');
 });
 
 app.listen(PORT, () => {
